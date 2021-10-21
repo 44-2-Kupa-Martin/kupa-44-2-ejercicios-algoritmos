4b5ec23e6ef4f5c4d4eea5df6b3b3a8b25608e59
tomas <tomitohuelmo@gmail.com>
2021-10-21 11:42:07 -0300

    .

---
+++ arrays/ej15.c
@@ -0,0 +1,45 @@
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    srand(time(NULL));
+    char *type[4]= {"copas", "basto", "espada", "oro"};
+    char *number[10]= {"Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Diez", "Once", "Doce"};
+    char deck[40][100]= {'\0'};
+    char *randDeck[40];
+    int deckIndex= 0;
+    int randIndexes[40]= {0};
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 10; j++) {
+          char card[100] = "";
+          strcat(card, number[j]);
+          strcat(card, " de ");
+          strcat(card, type[i]);
+          strcat(deck[deckIndex], card);
+          deckIndex++;
+        }
+    }
+    int i= 1;
+    while (i < 40) {
+      int randNum= rand()%40;
+      bool isUnique= true;
+      if (randIndexes[i-1] == randNum) {
+        isUnique= false;
+      }
+      if (isUnique) {
+        randIndexes[i]= rand()%40;
+        i++;
+      }
+    }
+    for (size_t i = 0; i < 40; i++) {
+      randDeck[i]= deck[randIndexes[i]];
+      printf("%s\n", randDeck[i]);
+    }
+    return 0;
+  }
